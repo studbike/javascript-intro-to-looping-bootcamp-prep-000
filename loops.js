5ae5f3c730ae812312b03684847194c503085a99
grant diamond <grant.a.diamond@gmail.com>
2018-03-27 19:38:35 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,3 +16,13 @@ function whileLoop(num){
     }
   }
 }
+function doWhileLoop(arr){
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+do {
+  arr.pop()
+} while (array.length > 0 && maybeTrue());
+return arr
+}
